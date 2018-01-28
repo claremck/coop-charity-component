7e56882b174016e09a20abceb76be8f276d7d011
clare <clare@pushon.co.uk>
2018-01-28 19:59:12 +0000

    get data from API

--- app/js/progress_bar.js
+++ app/js/progress_bar.js
@@ -1,21 +1,39 @@
 jQuery.noConflict();
 (function ($) {
-    function progressBar() {
-        var raised = $(".raised").text();
-		var total = $(".total").text();		
-		
-		var difference = total - raised;
-		var fraction = difference / total;
-		var percent = fraction * 100;
-		var percentRound = percent.toFixed();
-		var percentRound = percentRound + '%';
-							    
-			$('.bar').css('padding-left', percentRound);
-		
+	 function getApi() {
+        $.getJSON( "https://coop-mock-test-api.herokuapp.com", function( data ) {
+		  var items = [];
+		  $.each( data, function( key, val ) {
+			items.push( "<span id='" + key + "'>" + val + "</span>" );
+		  });
+		 
+		  $( "<span/>", {
+			"id": "amount-raised",
+			html: items.join( "" )
+		  }).appendTo( "body" );
+		  
+		  //progress bar
+		  
+			var raised = $("#raised").text();
+			var total = $("#target").text();		
+			
+			var fraction = raised / total;
+			var percent = fraction * 100;
+			var percentRound = percent.toFixed();
+			var percentRound = percentRound + '%';
+									
+				$('.bar').css('padding-left', percentRound);
+				$('#raised').appendTo('#amt_raised');
+				$('#target').appendTo('#amt_target');
+				
+				$('#amount-raised').remove();
+				  
+			});
+	
     }
 
     $(document).ready(function () {
-       progressBar();
+		getApi();  
     });
 
     $(window).resize(function () {
